d7d666df803e3793f602470c5a52a2484ce3fb0d
JB <kjb961013@snu.ac.kr>
2025-03-20 12:47:11 -0500

    commit

--- scripts/convert_trk_to_nii.sh
+++ scripts/convert_trk_to_nii.sh
@@ -8,7 +8,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/"
 
 
 for tck_file in $( ls ${tcks}/*.tck ); do
+    echo $tck_file
     base_name=$(basename "${tck_file}" .tck)
-    output_file="$SCRIPT_DIR/${tractmasks_folder}/${base_name}.nii.gz"
+    output_file="${tractmasks_folder}/${base_name}.nii.gz"
     python trk_2_binary.py "${tck_file}" "${output_file}" "${REFERENCE_FILE}"
 done
